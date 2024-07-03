4aaf20eb096692a2130416c2cec8aa28d7e0895d
skago <skagouse@yahoo.com>
2024-07-03 13:29:12 -0400

    First commit

--- src/test/java/Utilities/LoginUtility.java
+++ src/test/java/Utilities/LoginUtility.java
@@ -4,6 +4,11 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class LoginUtility {
 
@@ -14,6 +19,8 @@ public class LoginUtility {
         ChromeOptions co= new ChromeOptions();
         co.setBrowserVersion("126");
         driver = new ChromeDriver(co);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10);
         driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin");
 
 
@@ -21,6 +28,22 @@ public class LoginUtility {
         driver.manage().window().maximize();
 
     }
+    public static void launchBrowserfromAWS(){
+        DesiredCapabilities capabilities= new DesiredCapabilities();
+        capabilities.setBrowserName("chrome");
+        //ChromeOptions co= new ChromeOptions();
+        //co.setBrowserVersion("126");
+        driver= new RemoteWebDriver(new URL("http://localhost:4444"),capabilities);
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.get("https://www.linkedin.com/login?fromSignIn=true&trk=guest_homepage-basic_nav-header-signin");
+
+
+        //Maximize the browser
+        driver.manage().window().maximize();
+
+    }
+
 
     public static void enterLoginCredentials() {
 
